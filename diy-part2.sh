b87756f7cdba9d3debe93cd988ef56b6359372ee
VALIDIY <104436461+VALIDIY@users.noreply.github.com>
2025-11-17 01:19:24 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,13 +10,11 @@
 # See /LICENSE for more information.
 #
 
-# 固件版本号添加个人标识和日期
-sed -i "s/DISTRIB_DESCRIPTION='.*ImmortalWrt '/DISTRIB_DESCRIPTION='FICHEN($(TZ=UTC-8 date +%Y.%m.%d))@VALIDIY ImmortalWrt '/g" package/emortal/default-settings/files/99-default-settings
-
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
+rm -rf feeds/luci/themes/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
 
 # Modify hostname
