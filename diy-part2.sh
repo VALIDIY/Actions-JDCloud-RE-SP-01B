e45df135681d8725a755f041a4ce11f9b38c7355
VALIDIY <104436461+VALIDIY@users.noreply.github.com>
2025-11-17 00:04:08 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,7 +17,7 @@ sed -i "s/DISTRIB_DESCRIPTION='.*ImmortalWrt '/DISTRIB_DESCRIPTION='FICHEN($(TZ=
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/themes/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
 
 # Modify hostname
 sed -i 's/ImmortalWrt/JDCloud_5G_CPE/g' package/base-files/files/bin/config_generate
