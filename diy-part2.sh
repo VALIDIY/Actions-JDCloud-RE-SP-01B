8550520f3f222de1b53bc21a87a414a9a5e200d1
VALIDIY <104436461+VALIDIY@users.noreply.github.com>
2025-11-17 12:22:51 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,10 +12,17 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+sed -i '/config interface '\''br-lan'\''/,/^$/s/option macaddr.*/option macaddr '\''DC:D8:7C:12:92:92'\''/' package/base-files/files/bin/config_generate
+sed -i '/config interface '\''lan1'\''/,/^$/s/option macaddr.*/option macaddr '\''DC:D8:7C:12:92:92'\''/' package/base-files/files/bin/config_generate
+sed -i '/config interface '\''lan2'\''/,/^$/s/option macaddr.*/option macaddr '\''DC:D8:7C:12:92:92'\''/' package/base-files/files/bin/config_generate
+sed -i '/config interface '\''wan'\''/,/^$/s/option macaddr.*/option macaddr '\''DC:D8:7C:12:92:93'\''/' package/base-files/files/bin/config_generate
+sed -i '/config interface '\''eth0'\''/,/^$/s/option macaddr.*/option macaddr '\''DC:D8:7C:12:92:93'\''/' package/base-files/files/bin/config_generate
+
+# Wi-Fi name
+sed -i 's/OpenWrt/JDCloud/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # Modify default theme
-rm -rf feeds/luci/themes/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 git clone https://github.com/VALIDIY/luci-app-5GSmartCase.git package/luci-app-5GSmartCase
 
 # Modify hostname
