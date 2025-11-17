b45b64af89e378b987ba8ad8789537ed8dc73928
VALIDIY <104436461+VALIDIY@users.noreply.github.com>
2025-11-17 13:20:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,14 +12,9 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
-sed -i '/config interface '\''br-lan'\''/,/^$/s/option macaddr.*/option macaddr '\''DC:D8:7C:12:92:92'\''/' package/base-files/files/bin/config_generate
-sed -i '/config interface '\''lan1'\''/,/^$/s/option macaddr.*/option macaddr '\''DC:D8:7C:12:92:92'\''/' package/base-files/files/bin/config_generate
-sed -i '/config interface '\''lan2'\''/,/^$/s/option macaddr.*/option macaddr '\''DC:D8:7C:12:92:92'\''/' package/base-files/files/bin/config_generate
-sed -i '/config interface '\''wan'\''/,/^$/s/option macaddr.*/option macaddr '\''DC:D8:7C:12:92:93'\''/' package/base-files/files/bin/config_generate
-sed -i '/config interface '\''eth0'\''/,/^$/s/option macaddr.*/option macaddr '\''DC:D8:7C:12:92:93'\''/' package/base-files/files/bin/config_generate
 
 # Wi-Fi name
-sed -i 's/set wireless\.default_\${name}\.ssid=ImmortalWrt/set wireless.default_\${name}.ssid=JDCloud/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/ImmortalWrt/JDCloud/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
 
 # Modify default theme
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
